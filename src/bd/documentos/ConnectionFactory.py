6f77bc6306d5939ab981c7a9ae577ff937c29594
Eduardo Lima <eduardoolima@Eduardos-MacBook.local>
2020-07-15 19:25:13 -0300

    Insert no mongoDB funcionando

--- src/bd/documentos/ConnectionFactory.py
+++ src/bd/documentos/ConnectionFactory.py
@@ -5,11 +5,13 @@ class ConnectionFactory:
     HOST = "localhost"
     PORT = 27017
     DB = "easypark"
+    USERNAME = "ep3_documento"
+    PASSWORD = "ep3_documento"
 
     connection_factory = None
 
     def __init__(self):
-        self.database = MongoClient(ConnectionFactory.HOST, ConnectionFactory.PORT)[ConnectionFactory.DB]
+        self.database = MongoClient(ConnectionFactory.HOST, ConnectionFactory.PORT, username=ConnectionFactory.USERNAME, password=ConnectionFactory.PASSWORD)[ConnectionFactory.DB]
 
     def get_collection(self, name):
         return self.database[name]
